e7123f0af6289cc6abf02b3f7a1c1acb54fa67b7
Dylan <gouindylan7@gmail.com>
2020-11-16 18:09:43 +0100

    new func try_me

--- my_test_package/lib.py
+++ my_test_package/lib.py
@@ -48,6 +48,9 @@ def clean_data(data):
     return data
 
 
+def try_me():
+    print('being inventive')
+
 if __name__ == '__main__':
     # For introspections purpose to quickly get this functions on ipython
     import my_test_package
